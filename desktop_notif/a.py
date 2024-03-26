4175b8a4a12463809223ce1039b020df440dbbfa
th3x3rx3s <kecskes.gergo0829@gmail.com>
2024-03-26 16:49:12 +0100

    Mappa törlése master branchből

--- desktop_notif/a.py
+++
@@ -1,4 +0,0 @@
-import requests as r
-url='https://raw.githubusercontent.com/th3x3rx3s/Python/master/test.json'
-url=r.get(url)
-print(url.json()['coord']['lat'])
--- desktop_notif/notif2.py
+++
@@ -1,7 +0,0 @@
-import notify2
-n = notify2.Notification(
-    "Cím",
-    "Szövegtörzs",
-    "bell.ico"
-)
-n.show()
