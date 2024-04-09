0c4d580613d5d9bb8e66e28290d1174beea592ea
th3x3rx3s <kecskes.gergo0829@gmail.com>
2024-04-09 19:32:56 +0200

    Javítva: a kód átláthatóbb lett.

--- Számrendszer átváltó/number_base_converter.py
+++ Számrendszer átváltó/number_base_converter.py
@@ -1,24 +1,44 @@
+def convert_to_decimal(number, base):
+    n=len(str(number))-1
+    decimal=0
+    for x in str(number):
+        decimal+=int(x)*(base**n)
+        n=n-1
+    return decimal
+
+def is_base_correct(base):
+    if 2<=base<=36:
+        return True
+    else:
+        return False
+
+def is_number_correct(num,base):
+    for x in str(num):
+        if int(x)>=base:
+            return False
+    return True
+    
 def base_converter(num,ogbase,tgbase):
     digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if ogbase<2 or ogbase>36 or tgbase<2 or tgbase>36:
+    #megvizsgálja, hogy a bevitt számrendszerek 2-36 között vannak-e
+    if is_base_correct(ogbase)==False or is_base_correct(tgbase)==False:
         return "Hiba: Az eredeti és cél számrendszerek csak 2 és 36 között lehetnek."
+    
     if num<0:
         return "Hiba: A szám nem lehet negatív."
-    for x in str(num):
-        if int(x)>=ogbase:
-            return f"Hiba: {ogbase} alapú számrendszerben nem szerepelhet ilyen szám(0-{ogbase-1})."
+    
+    #megvizsgálja, hogy a bevitt szám lehetséges-e az adott számrendszerben
+    if is_number_correct(num,ogbase)==False:
+        return f"Hiba: {ogbase} alapú számrendszerben nem szerepelhet ilyen szám(0-{ogbase-1})."
+    
     result=""
     if ogbase!=10 and tgbase!=10:
         #decimális számrendszerbe váltás
-        n=len(str(num))-1
-        decimal=0
-        for x in range(len(str(num))):
-            decimal+=(int(str(num)[x])*(ogbase**n))
-            n=n-1
+        dec=convert_to_decimal(num,ogbase)
         #decimálisból cél számrendszerbe váltás
-        while decimal>0:
-            result+=digits[decimal%tgbase]
-            decimal=decimal//tgbase
+        while dec>0:
+            result+=digits[dec%tgbase]
+            dec=dec//tgbase
         return result[::-1]
     elif ogbase==10:
         #decimálisból cél számrendszerbe váltás
@@ -28,11 +48,8 @@ def base_converter(num,ogbase,tgbase):
         return result[::-1]
     elif tgbase==10:
         #eredeti számrendszerből decimálisba váltás
-        n=len(str(num))-1
-        decimal=0
-        for x in range(len(str(num))):
-            decimal+=(int(str(num)[x])*(ogbase**n))
-            n=n-1
-        return decimal
+        dec=convert_to_decimal(num,ogbase)
+        return dec
     elif ogbase==tgbase:
         return num
+print(base_converter(110,2,37))
