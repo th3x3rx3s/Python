660978e91fef5a88f65dfdf8521ff537c9f43c7b
th3x3rx3s <kecskes.gergo0829@gmail.com>
2024-04-10 20:41:01 +0200

    Frissítve: mostmár importálható a program más programokban, hogy ott is használni lehessen

--- Naptár/calenda.py
+++ Naptár/calenda.py
@@ -9,29 +9,29 @@ def szokoev(year):
     if year%4==0: return True
     else: return False
 
-date=dt.now()
-
-months_howmanydays=[]
-#megnézi, hogy hány napból áll havonta az év
-if szokoev(date.year):
-    months_howmanydays=[31,29,31,30,31,30,31,31,30,31,30,31]
-else:
-    months_howmanydays=[31,28,31,30,31,30,31,31,30,31,30,31]
-
-print(f"\t\t\t{months[date.month-1]}\t\t\t")
-
-for x in days:
-    if x==days[-1]: print(f"{x}", end="")
-    else: print(f"{x}\t", end="")
-print()
-
-#"\t"*(dt.isocalendar(dt.strptime(f"{date.year} {date.month} {y+1}", "%Y %m %d")).weekday-1) -> annyi tabulátort tegyen elé az első futásnál amilyen nappal kezd az a hónap(pl.: péntek -> 4 tabulátor)
-
-for y in range(months_howmanydays[date.month-1]):
-    if y==0 or what_day_it_is(f"{date.year} {date.month} {y+1}")=="Mon":
-        print("\t"*(dt.isocalendar(dt.strptime(f"{date.year} {date.month} {y+1}", "%Y %m %d")).weekday-1),y+1,end="")
-    elif what_day_it_is(f"{date.year} {date.month} {y+1}")=="Sun":
-        print("\t",y+1,end="\n")
+def Naptar():
+    date=dt.now()
+    months_howmanydays=[]
+    #megnézi, hogy hány napból áll havonta az év
+    if szokoev(date.year):
+        months_howmanydays=[31,29,31,30,31,30,31,31,30,31,30,31]
     else:
-        print("\t",y+1,end="")
+        months_howmanydays=[31,28,31,30,31,30,31,31,30,31,30,31]
+
+    print(f"\t\t\t{months[date.month-1]}\t\t\t")
+
+    for x in days:
+        if x==days[-1]: print(f"{x}", end="")
+        else: print(f"{x}\t", end="")
+    print()
+
+    #"\t"*(dt.isocalendar(dt.strptime(f"{date.year} {date.month} {y+1}", "%Y %m %d")).weekday-1) -> annyi tabulátort tegyen elé az első futásnál amilyen nappal kezd az a hónap(pl.: péntek -> 4 tabulátor)
+
+    for y in range(months_howmanydays[date.month-1]):
+        if y==0 or what_day_it_is(f"{date.year} {date.month} {y+1}")=="Mon":
+            print("\t"*(dt.isocalendar(dt.strptime(f"{date.year} {date.month} {y+1}", "%Y %m %d")).weekday-1),y+1,end="")
+        elif what_day_it_is(f"{date.year} {date.month} {y+1}")=="Sun":
+            print("\t",y+1,end="\n")
+        else:
+            print("\t",y+1,end="")
     
