d9638dbd6d81c121aaad955bc237b83cc5d05026
th3x3rx3s <kecskes.gergo0829@gmail.com>
2024-04-19 17:45:13 +0200

    Javítva: Mostmár 16-osból is lehet váltani

--- Számrendszer átváltó/number_base_converter.py
+++ Számrendszer átváltó/number_base_converter.py
@@ -1,8 +1,9 @@
+digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def convert_to_decimal(number, base):
     n=len(str(number))-1
     decimal=0
     for x in str(number):
-        decimal+=int(x)*(base**n)
+        decimal+=digits.index(x)*(base**n)
         n=n-1
     return decimal
 
@@ -14,22 +15,21 @@ def is_base_correct(base):
 
 def is_number_correct(num,base):
     for x in str(num):
-        if int(x)>=base:
+        if digits.index(x)>=base:
             return False
     return True
     
 def base_converter(num,ogbase,tgbase):
-    digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    #megvizsgálja, hogy a bevitt számrendszerek 2-36 között vannak-e
+    #megvizsgálja, hogy a bevitt számrendszerek 2 és 36 között vannak-e
     if is_base_correct(ogbase)==False or is_base_correct(tgbase)==False:
         return "Hiba: Az eredeti és cél számrendszerek csak 2 és 36 között lehetnek."
     
-    if num<0:
-        return "Hiba: A szám nem lehet negatív."
-    
     #megvizsgálja, hogy a bevitt szám lehetséges-e az adott számrendszerben
     if is_number_correct(num,ogbase)==False:
-        return f"Hiba: {ogbase} alapú számrendszerben nem szerepelhet ilyen szám(0-{ogbase-1})."
+        return f"Hiba: {ogbase} alapú számrendszerben nem szerepelhet ilyen szám[0-{ogbase-1}]."
+    
+    if int(ogbase)<=10 and int(num)<0:
+        return "Hiba: A szám nem lehet negatív."
     
     result=""
     if ogbase!=10 and tgbase!=10:
@@ -52,4 +52,4 @@ def base_converter(num,ogbase,tgbase):
         return dec
     elif ogbase==tgbase:
         return num
-print(base_converter(110,2,37))
+print(base_converter("AC4571B",15,16))
